e1f48fbfbcbe5dfa4c3c8398ebe0906651da0505
Rahul Garud <84452921+Rahul123223@users.noreply.github.com>
2021-12-18 01:09:07 +0530

    Add files via upload

---
+++ signIn.js
@@ -0,0 +1,17 @@
+document.querySelector("button").addEventListener("click", signIn);
+var regUsers = JSON.parse(localStorage.getItem("userData"));
+console.log("regUsers:", regUsers);
+
+function signIn() {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+
+  for (var i = 0; i < regUsers.length; i++) {
+    if (regUsers[i].emailAddress == email && regUsers[i].passWord == password) {
+      window.location.href = "Home.html";
+      
+    } else {
+      alert("Wrong Email ID & Password");
+    }
+  }
+}
