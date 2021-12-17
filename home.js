39a3db19edfda7e45ef5de94bbbc62d26c59e7a7
Rahul Garud <84452921+Rahul123223@users.noreply.github.com>
2021-12-18 01:08:01 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,15 @@
+function signIn() {
+  window.location.href = "signIn.html";
+}
+function createAcc() {
+  window.location.href = "createAcc.html";
+}
+function tripS() {
+  window.location.href = "trips.html";
+}
+ document.querySelector("btn").addEventListener("click",createBtn);
+
+function createBtn(){
+    window.location.href="product.html"
+}
+
